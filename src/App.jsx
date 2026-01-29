755e000e0725e5e2e00eb27656cffe4313ca6d22
Zaur <zaurik87@gmail.com>
2026-01-29 19:50:15 +0400

    İcon üzərinə mousu gətirdikdə onun aktiv olması. Bunun üçün activeİtem useState yaradılır. onun ilkin dəyəri null verilir "" də verə bilərik. daha sonrahandleMouseEnter funksiyası bu funksiyanın parametrinə iconun adı veriləcək və verilmiş ad setActiveİtem vasitəsilə mənimsədiləcək. bu funksiyanın məqsədi mouseni lelemnt üzərinə gətirəndə active klasının aktiv olmasıdır. 2ci funksiya isə handleMouseLeave adlanır bu funksiya isə mouseni elemnt üzərindən çıxaranda ona null dəyəri mənimsədəcək hər 2 funksiyanı img tegində istifadə edirik. onMouseEnter funksiyasına src-də yazılan adları veririk.  className-də dəyişiklik edirik oraya əlavə olaraq activeİtemdən gələn adları verməklə yoxlama aparırıq həmin addırsa active klası aktiv olacaq

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 function App() {
   const [count, setCount] = useState(0);
   const [isOpen, setIsOpen] = useState(false);
+  const [activeItem, setActiveItem] = useState(null);
 
   function handleIncrement() {
     setCount((prev) => prev + 1);
@@ -20,6 +21,14 @@ function App() {
     setIsOpen((prev) => !prev);
   }
 
+  function handleMouseEnter(item) {
+    setActiveItem(item);
+  }
+
+  function handleMouseLeave() {
+    setActiveItem(null); 
+  }
+
   console.log(isOpen);
 
   return (
@@ -35,20 +44,25 @@ function App() {
           <h1>Vite + React = {count >= 3 ? "Love" : ""}</h1>
           <div className="logo-container">
             <img
+              onMouseEnter={() => handleMouseEnter("vite")}
+              onMouseLeave={handleMouseLeave}
               src="/vite.svg"
-              className={`logo ${count >= 1 ? "active" : ""}`}
+              className={`logo ${count >= 1 || activeItem === "vite" ? "active" : ""}`}
               alt="Vite logo"
             />
             <p>+</p>
             <img
+              onMouseEnter={() => handleMouseEnter("react")}
               src="/react.svg"
-              className={`logo ${count >= 2 ? "active" : ""}`}
+              className={`logo ${count >= 2 || activeItem === "react" ? "active" : ""}`}
               alt="React logo"
             />
             <p>=</p>
             <img
+              onMouseEnter={() => handleMouseEnter("love")}
+              onMouseLeave={handleMouseLeave}
               src="/love.svg"
-              className={`logo ${count >= 3 ? "active" : ""}`}
+              className={`logo ${count >= 3 || activeItem === "love" ? "active" : ""}`}
               alt="Love logo"
             />
           </div>
