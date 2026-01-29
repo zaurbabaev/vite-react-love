0c0c5bb2ca4bdbc0ff0975015f398674765e53f5
Zaur <zaurik87@gmail.com>
2026-01-29 19:16:29 +0400

    create function toggleIsOpen where we open the window when click button, and close window when click x

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
 
   function handleIncrement() {
     setCount((prev) => prev + 1);
@@ -14,31 +15,54 @@ function App() {
   function handleReset() {
     setCount(0);
   }
+
+  function toggleIsOpen() {
+    setIsOpen((prev) => !prev);
+  }
+
+  console.log(isOpen);
+
   return (
     <>
-      {/* <span className="cross">&times;</span> */}
-      {/* <button>Начать</button> */}
+      {isOpen ?
+        <span onClick={toggleIsOpen} className="cross">
+          &times;
+        </span>
+      : <button onClick={toggleIsOpen}>Начать</button>}
 
-      <h1>Vite + React = Love</h1>
-
-      {/* <div className="logo-container">
-        <img src="/vite.svg" className="logo" alt="Vite logo" />
-        <p>+</p>
-        <img src="/react.svg" className="logo" alt="React logo" />
-        <p>=</p>
-        <img src="/love.svg" className="logo" alt="Love logo" />
-      </div> */}
-
-      <hr />
-
-      <div className="card">
-        <p className="count-paragraph">count is {count}</p>
-        <div className="increment-buttons">
-          <button onClick={handleIncrement}>+1</button>
-          <button onClick={handleDecrement}>-1</button>
-          <button onClick={handleReset}>Reset</button>
-        </div>
-      </div>
+      {isOpen && (
+        <>
+          <h1>Vite + React = {count >= 3 ? "Love" : ""}</h1>
+          <div className="logo-container">
+            <img
+              src="/vite.svg"
+              className={`logo ${count >= 1 ? "active" : ""}`}
+              alt="Vite logo"
+            />
+            <p>+</p>
+            <img
+              src="/react.svg"
+              className={`logo ${count >= 2 ? "active" : ""}`}
+              alt="React logo"
+            />
+            <p>=</p>
+            <img
+              src="/love.svg"
+              className={`logo ${count >= 3 ? "active" : ""}`}
+              alt="Love logo"
+            />
+          </div>
+          <hr />
+          <div className="card">
+            <p className="count-paragraph">count is {count}</p>
+            <div className="increment-buttons">
+              <button onClick={handleIncrement}>+1</button>
+              <button onClick={handleDecrement}>-1</button>
+              <button onClick={handleReset}>Reset</button>
+            </div>
+          </div>
+        </>
+      )}
     </>
   );
 }
